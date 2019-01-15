c8f7f96c85133f2370c7cb6a71d196aef8d5c924
yungjin <chyj0318@naver.com>
2019-01-15 10:32:16 +0900

    ㅇㅇ

--- Program.cs
+++ Program.cs
@@ -19,6 +19,6 @@ namespace web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://*:80");
     }
 }
